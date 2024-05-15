ac475b9c2514dc1275df9522f06fa3228a16faf0
Otávio Biagioni <otaviobiagioni31@gmail.com>
2024-05-15 18:43:55 -0300

    Organizei por Pastas

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -19,29 +19,28 @@ function stickyHeader() {
 var modal = document.querySelector(".modal-olimpiada");
 
 // Get the <span> element that closes the modal
-var span = document.querySelector(".close");
+var botaoFecharModal = document.querySelector(".close");
 
 var body = document.querySelector("body");
 
-// When the user clicks on the button, open the modal
 function abrirModal() {
   modal.style.display = "block";
-  body.classList.add("stop-scrolling");
+  body.classList.add("bloquear-scroll");
   header.style.display = "none";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+// Quando usuário clicar no span X, feche o modal
+botaoFecharModal.onclick = function() {
   modal.style.display = "none";
-  body.classList.remove("stop-scrolling");
+  body.classList.remove("bloquear-scroll");
   header.style.display = "flex";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+// Quando usuário clicar fora do modal, feche-o
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
-    body.classList.remove("stop-scrolling");
+    body.classList.remove("bloquear-scroll");
     header.style.display = "flex";
   }
 }
