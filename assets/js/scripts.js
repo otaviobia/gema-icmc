c97bac12936220d6882dad09ade10164a7aff309
Otávio Biagioni <otaviobiagioni31@gmail.com>
2024-05-15 20:05:20 -0300

    Consertei Identação
    2 espaços ao invés de 4

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,24 +1,21 @@
 window.onscroll = function() { stickyHeader() };
 
-var header = document.querySelector(".header");
+var header = document.querySelector("header");
 
 var sticky = header.offsetTop;
 
 function stickyHeader() {
-    if (window.scrollY > sticky)
-    {
-        header.classList.add("sticky");
-    }
-    else 
-    {
-        header.classList.remove("sticky");
-    }
+  if (window.scrollY > sticky)
+  {
+    header.classList.add("sticky");
+  }
+  else 
+  {
+    header.classList.remove("sticky");
+  }
 }
 
-// Get the modal
 var modal = document.querySelector(".modal-olimpiada");
-
-// Get the <span> element that closes the modal
 var botaoFecharModal = document.querySelector(".close");
 
 var body = document.querySelector("body");
@@ -29,7 +26,6 @@ function abrirModal() {
   header.style.display = "none";
 }
 
-// Quando usuário clicar no span X, feche o modal
 botaoFecharModal.onclick = function() {
   modal.style.display = "none";
   body.classList.remove("bloquear-scroll");
